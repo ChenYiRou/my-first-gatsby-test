07dc827accaaf37390758887c7c56dd10b22f1c6
Chen Yi Rou <atirchen0011001@gmail.com>
2024-12-08 17:32:48 +0800

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const indexPage = () => {
   return (
@@ -8,6 +8,11 @@ const indexPage = () => {
       <h1>Welcome to my Gatsby site!</h1>
       <Layout pageTitle="Home Page">
         <p>I''m making this. by following the Gatsby tutorial!</p>
+
+        <StaticImage
+          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+          src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        />
       </Layout>
     </main>
   );
